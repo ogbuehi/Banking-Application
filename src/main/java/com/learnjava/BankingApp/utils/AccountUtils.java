53309af70b0304c26d68519d0150da78170e8e99
ogbuehi <160658459+ogbuehi@users.noreply.github.com>
2024-07-23 13:24:47 +0100

    Update AccountUtils.java

--- src/main/java/com/learnjava/BankingApp/utils/AccountUtils.java
+++ src/main/java/com/learnjava/BankingApp/utils/AccountUtils.java
@@ -20,46 +20,4 @@ public class AccountUtils {
         String number = String.valueOf(randomNumber);
         return year + number;
     }
-    // public static boolean verifyEmail(String email){
-    //     return true;
-    // }
-    public static String generatePassword(String password){
-        // generate a string of upper case letters having length 2  
-        String upperCaseStr = RandomStringUtils.random(2, 65, 90, true, true);  
-         
-        // generate a string of lower case letters having length 2  
-        String lowerCaseStr = RandomStringUtils.random(2, 97, 122, true, true);  
-          
-        // generate a string of numeric letters having length 2  
-        String numbersStr = RandomStringUtils.randomNumeric(2);  
-          
-        // generate a string of special chars having length 2  
-        String specialCharStr = RandomStringUtils.random(2, 33, 47, false, false);  
-          
-        // generate a string of alphanumeric letters having length 2  
-        String totalCharsStr = RandomStringUtils.randomAlphanumeric(2);  
-          
-        // concatenate all the strings into a single one  
-        String demoPassword = upperCaseStr.concat(lowerCaseStr)  
-          .concat(numbersStr)  
-          .concat(specialCharStr)  
-          .concat(totalCharsStr);  
-          
-        // create a list of Char that stores all the characters, numbers and special characters   
-        List<Character> listOfChar = demoPassword.chars()  
-                .mapToObj(data -> (char) data)  
-                .collect(Collectors.toList());  
-          
-        // use shuffle() method of the Collections to shuffle the list elements   
-        Collections.shuffle(listOfChar);  
-          
-        //generate a random string(secure password) by using list stream() method and collect() method  
-        String password = listOfChar.stream()  
-                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)  
-                .toString();  
-                  
-        // return RandomStringGenerator password to the main() method   
-        return password;  
-    }
-
 }
